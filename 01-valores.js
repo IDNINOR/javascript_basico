36582c691c3ec0b6a826661e056ed46ce4f24eac
Israel Daniel Niño Rodriguez <idninor4@gmail.com>
2023-08-23 16:19:01 -0500

    Comienzo de JavaScript Básico

---
+++ 01-valores.js
@@ -0,0 +1,16 @@
+//VALORES PRIMITIVOS
+
+//NUMERICO
+40
+//TEXTO//
+"Diego de Granda"
+// BOOLEANOS
+true
+false
+// NULLO
+null
+undefined
+
+//VALORES NO PRIMITIVOS
+[1, 2, 3]
+{ nombre : "lucas"}
