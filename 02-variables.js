1ea6cf9367bcae1affa9e25e7a4dff344ea34021
Israel Daniel Niño Rodriguez <idninor4@gmail.com>
2023-08-23 17:02:22 -0500

    Variables

---
+++ 02-variables.js
@@ -0,0 +1,22 @@
+//Variables
+var nombre = "Oscar";
+
+/**
+ *  Estados de una variable
+ *  declarar
+ */
+
+var edad;
+
+//Inicializar
+
+edad = 30;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+persona;
